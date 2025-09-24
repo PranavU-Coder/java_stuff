670f02fd9201ef3c7fab156aef3eede6ea4ba06a
Pranav <pranavu8406@gmail.com>
2025-09-24 09:34:40 +0530

    Create School.java

---
+++ comCourseStructure/School.java
@@ -0,0 +1,22 @@
+package comCourseStructure;
+
+public class School extends Building {
+    private int classrooms;
+    private String gradeLevel;
+
+    public int getClassrooms() {
+        return classrooms;
+    }
+
+    public void setClassrooms(int classrooms) {
+        this.classrooms = classrooms;
+    }
+
+    public String getGradeLevel() {
+        return gradeLevel;
+    }
+
+    public void setGradeLevel(String gradeLevel) {
+        this.gradeLevel = gradeLevel;
+    }
+}
