fd2215f6b3585f71b6fba2dcca7fbd18fde411fa
Pranav <pranavu8406@gmail.com>
2025-09-24 09:33:48 +0530

    Create Building.java

---
+++ comCourseStructure/Building.java
@@ -0,0 +1,22 @@
+package comCourseStructure;
+
+public class Building {
+    private double squareFootage;
+    private int stories;
+
+    public double getSquareFootage() {
+        return squareFootage;
+    }
+
+    public void setSquareFootage(double squareFootage) {
+        this.squareFootage = squareFootage;
+    }
+
+    public int getStories() {
+        return stories;
+    }
+
+    public void setStories(int stories) {
+        this.stories = stories;
+    }
+}
